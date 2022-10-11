d39583b9c7b34da19ca75ae37a2f89e3ef56b194
1E10 Laptop <sample@bths.edu>
2022-10-11 10:58:19 -0400

    e

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args){
+        String str1 = "This is my string!";
+        String str2 = "This is my string!";
+        String str3 = new String("This is my string!");
+        String str4 = new String("This is my string!");
+
+        System.out.println(str1.equals(str2));
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str3));
+        System.out.println(str3 == str4);
+        System.out.println(str3.equals(str4));
+
+    }
 }
