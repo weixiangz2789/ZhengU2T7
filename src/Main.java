cf50ba564e426cf9c281403ac0fefa4222d386a2
1E10 Laptop <sample@bths.edu>
2022-10-12 10:49:47 -0400

    e

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,42 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        String str1 = "This is my string!";
-        String str2 = "This is my string!";
-        String str3 = new String("This is my string!");
-        String str4 = new String("This is my string!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String str1 = scan.nextLine();
 
-        System.out.println(str1.equals(str2));
-        System.out.println(str1 == str2);
-        System.out.println(str1.equals(str3));
-        System.out.println(str3 == str4);
-        System.out.println(str3.equals(str4));
+        System.out.println("String length: " + str1.length());
+        System.out.println("First half: " + str1.substring(0,(str1.length()/2)));
+        System.out.println("Second half: " + str1.substring(str1.length()/2));
 
+        System.out.print("Enter second string: ");
+        String str2 = scan.nextLine();
+
+        if (str1.length() > str2.length()){
+            System.out.println(str1 + " is longer");
+        }
+        if (str1.length() < str2.length()){
+            System.out.println(str2 + " is longer");
+        }
+        if (str1.length() == str2.length()){
+            System.out.println("Both strings have the same length");
+        }
+        if (str1.equals(str2)){
+            System.out.println("Both strings have the same exact characters");
+        }
+        int temp = str1.compareTo(str2);
+        if (temp > 0){
+            System.out.println(str2 + " is first alphabetically");
+        }
+        if (temp < 0){
+            System.out.println(str1 + " is first alphabetically");
+        }
+        int index = str1.indexOf(str2);
+        if (index == -1){
+            System.out.println(str2 + " is not found in " + str1);
+        }
+        else {
+            System.out.println(str2 + " is found in " + str1 + " at index " + index);
+        }
     }
 }
